bbbc33d64aae17291a351525375e986589b13676
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-11-24 07:19:34 +0200

    add router / redux / bootstrap

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Layout from './ui/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+      </Layout>
+    );
+  }
+}
+
+export default App;
